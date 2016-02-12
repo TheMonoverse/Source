be11821c01feed81a42df3652cf5b6b094388b13
zacharymacleod <zachary.macleod@gmail.com>
2016-02-12 13:10:03 -0500

    Initial Stuff
    from the GD

---
+++ Assets/test.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class test : MonoBehaviour {
+    Vector2 offset;
+    // Use this for initialization
+    void Start () {
+        offset = new Vector2(0f,1f);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+       offset.y = Mathf.Max(0.5f,offset.y-(0.1f * Time.deltaTime));
+
+       // GetComponent<Camera>().rect = new Rect(new Vector2(0, 0), new Vector2(1,0.25f));
+
+        
+        // setup the rectangle
+        GetComponent<Camera>().rect = new Rect(offset.x, offset.y, 1 , 0.5f);
+    }
+}
