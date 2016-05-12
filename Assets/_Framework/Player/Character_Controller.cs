193176462437c4487fa34c54bc69aae9999dd90c
LeoQuint <leonardquintal@gmail.com>
2016-05-11 22:19:19 -0400

    Ready to create mock levels

--- Assets/_Framework/Player/Character_Controller.cs
+++ Assets/_Framework/Player/Character_Controller.cs
@@ -2,25 +2,25 @@
 using System.Collections;
 
 public class Character_Controller : MonoBehaviour {
-	//hello
+	
+    //Character's name
     public string mName;
 
+    
+
+    ///Physics Variables
     Rigidbody2D rb;
-
-    GameController gc;
-
     private float move;
 
-
-    /// <summary>
     /// Ground Check Variables
-    /// </summary>
-    public LayerMask groundLayer;//layer that is considered grounded. 
+    public LayerMask groundLayer;//layer the player needs to touch in order to jump.
     private bool grounded = false;
     private float groundRadius = 0.1f;
     [SerializeField]
     Transform groundCheck;
-    //----//
+
+    ///Script references
+    GameController gc; //
 
     void Awake() 
     {
@@ -47,16 +47,16 @@ public class Character_Controller : MonoBehaviour {
     void PlayerInput() 
     {
         move = Input.GetAxis("Horizontal");
-        float jump = 0f;
+        
         if (move > 0.1f || move < -0.1f)
         {
             rb.AddForce( new Vector2( move * 20f , 0f) );
             rb.velocity = new Vector3(Mathf.Clamp(rb.velocity.x, -5f, 5f), Mathf.Clamp(rb.velocity.y, -15f, 5f), 0f);
         }
-        if (Input.GetButtonDown("Jump"))
+        if (Input.GetButtonDown("Jump") && grounded)
         {
             rb.AddForce(new Vector2(0f, 500f));
-            jump = 1f;
+            
         }
       
 
